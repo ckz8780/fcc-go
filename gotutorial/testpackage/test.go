d61e03e90e457903a15e72ac4620ecd953931c32
Chris Zielinski <ckz8780@gmail.com>
2024-07-07 14:41:41 -0400

    Add notes

--- gotutorial/testpackage/test.go
+++ gotutorial/testpackage/test.go
@@ -2,7 +2,14 @@ package testpackage
 
 import "fmt"
 
+// Public (exported) functions start with a capital letter
+// and are accessible from other packages
 func MyFunction() {
 	fmt.Println("Hello World")
 }
 
+// Private functions begin with a lowercase letter 
+// and are not accessible from other packages
+func myPrivateFunction() {
+	fmt.Println("Hello Private World")
+}
